3701abadca3c89ac0815fe38baebdc5b1e039c80
Gabriel <montjagabriel@gmail.com>
2020-02-13 17:07:46 +0200

    Completed reveal-on-scroll functionality

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,16 +1,23 @@
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 
 class RevealOnScroll {
 
-    constructor(){
-        this.itemsToReveal = document.querySelectorAll('.feature-item');
+    constructor(els, thresholdPercent){
+        this.thresholdPercent = thresholdPercent
+        this.itemsToReveal = els;
         this.hideInitially();
+        this.browserHeight = window.innerHeight;
         this.scrollThrottle = throttle(this.calCaller, 200).bind(this);
         this.events();
     }
 
     events(){
-        window.addEventListener('scroll', this.scrollThrottle)
+        window.addEventListener('scroll', this.scrollThrottle);
+        window.addEventListener('resize', debounce(() => {
+            console.log('resizee has ran');
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     calCaller(){
@@ -23,14 +30,16 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el){
-        console.log('element was calculated');
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
-        if (scrollPercent < 75){
-            el.classList.add("reveal-item--is-visible");
-            el.isRevealed = true;
-
-            if(el.isLastItem) {
-                window.removeEventListener('scroll',this.scrollThrottle);
+        if(window.scrollY + this.browserHeight > el.offsetTop){
+            console.log('element was calculated');
+            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100;
+            if (scrollPercent < this.thresholdPercent){
+                el.classList.add("reveal-item--is-visible");
+                el.isRevealed = true;
+    
+                if(el.isLastItem) {
+                    window.removeEventListener('scroll',this.scrollThrottle);
+                }
             }
         }
     }
