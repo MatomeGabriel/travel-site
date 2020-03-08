e3ad89a8d19cb2acbfde6c41fdcccb5d2c1e50e9
Gabriel <montjagabriel@gmail.com>
2020-03-08 13:14:35 +0200

    completed sticky header functionality

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,20 +1,26 @@
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 
 class StickyHeader {
 
 
     constructor(){
         this.sideHeader = document.querySelector('.site-header');
-        this.pageSections = document.querySelectorAll(".page-section");
+        this.pageSections = document.querySelectorAll('.page-section')
         this.browserHeight = window.innerHeight;
+        this.previousScrollY = window.scrollY
         this.events();
     }
 
     events(){
         window.addEventListener('scroll', throttle(() => this.runOnScroll(), 200));
+        window.addEventListener('resize', debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     runOnScroll(){
+        this.determineScrollDirection();
         if(window.scrollY > 60){
             this.sideHeader.classList.add('site-header--dark');
             console.log('Added Dark');
@@ -22,13 +28,35 @@ class StickyHeader {
             this.sideHeader.classList.remove('site-header--dark');
             console.log('Removed Dark');
         }
-        this.pageSections.forEach(el => this.calcSection(el));
+        this.pageSections.forEach( el => this.calSection(el))
     }
-    calcSection(el){
-        if(window.scrollY + window.innerHeight > el.offsetTop && window.scrollY < el.offsetTop+el.offsetHeight) {
-            let scrollPercent = el.getBoundingClientRect().y/windows.innerHeight
+
+    determineScrollDirection(){
+        if(window.scrollY > this.previousScrollY) {
+            this.scrollDirection = 'down'
+
+        } else {
+            this.scrollDirection = 'up'
+        }
+
+        this.previousScrollY = window.scrollY;
+        
+    }
+
+    calSection(el){
+       
+        if(window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop+ el.offsetHeight){
+            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight)*100;
+
+            if(scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 33 && this.scrollDirection == 'up'){
+                let matchingLink = el.getAttribute("data-matching-link");
+                document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach( el => el.classList.remove("is-current-link"));
+                document.querySelector(matchingLink).classList.add("is-current-link")
+            }
+
         }
     }
+
 
 }
 
